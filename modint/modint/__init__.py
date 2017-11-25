4499abb98b4d63b7f30262acc7a942e341f72bde
Shlomi Fish <shlomif@shlomifish.org>
2017-11-25 16:00:17 +0200

    Add more functionality.

--- modint/modint/__init__.py
+++ modint/modint/__init__.py
@@ -7,7 +7,12 @@ __version__ = '0.1.0'
 class ChineseRemainderConstructor:
     def __init__(self, bases):
         """Accepts a list of two integer bases."""
-        self.bases = bases
+        self._bases = bases
+        p = 1
+        for x in bases:
+            p *= x
+        self._prod = p
+        self._inverses = [p//x for x in bases]
 
     def rem(self, mods):
         """Accepts a list of corresponding modulos for the bases and
