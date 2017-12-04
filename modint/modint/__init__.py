eb572d7feb468fae024448a577d04ac07006fb23
Shlomi Fish <shlomif@shlomifish.org>
2017-12-04 16:25:16 +0200

    implement the algo - todo: test.

--- modint/modint/__init__.py
+++ modint/modint/__init__.py
@@ -4,6 +4,7 @@ __author__ = """Shlomi Fish"""
 __email__ = 'shlomif@shlomifish.org'
 __version__ = '0.1.0'
 
+
 class ChineseRemainderConstructor:
     def __init__(self, bases):
         """Accepts a list of two integer bases."""
@@ -18,4 +19,20 @@ class ChineseRemainderConstructor:
         """Accepts a list of corresponding modulos for the bases and
         returns the accumulated modulo.
         """
-        return 1
+        ret = 0
+        for base, inv, mod in zip(self._bases, self._inverses, mods):
+            ret += mod * inv * self.mul_inv(inv, base)
+        return ret % self._prod
+
+    def mul_inv(self, a, b):
+        """Internal method that implements Euclid's modified gcd algorithm.
+        """
+        initial_b = b
+        x0, x1 = 0, 1
+        if b == 1:
+            return 1
+        while a > 1:
+            div, mod = divmod(a, b)
+            a, b = b, mod
+            x0, x1 = x1 - div * x0, x0
+        return (x1 if x1 >= 0 else x1 + initial_b)
